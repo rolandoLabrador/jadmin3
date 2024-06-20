f7ffcb4b1001accc48750d4c2985ac178d03afaf
Roland Labrador <rolando1515@hotmail.com>
2024-06-20 18:52:00 -0400

    commit

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -3,9 +3,10 @@
 // @ts-ignore
 'use client';
 import 'hero-slider/dist/index.css';
-import HeroSlider, { Nav, Overlay, Slide } from 'hero-slider';
-
-
+import HeroSlider, { Nav, Overlay, Slide ,MenuNav} from 'hero-slider';
+const firstB = 'https://images.unsplash.com/photo-1494475673543-6a6a27143fc8?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+const secondB ='https://images.unsplash.com/photo-1507525428034-b723cf961d3e';
+const thirdB = 'https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?q=80&w=1420&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 const Slider = () => {
   return (
     <HeroSlider
@@ -29,19 +30,23 @@ const Slider = () => {
     >
       {/* Add your slides here */}
 
+      <Overlay> 
 
+
+
+        
+      </Overlay>
 
       <Slide
         background={{
-          backgroundImageSrc:'https://images.unsplash.com/photo-1494475673543-6a6a27143fc8?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-         
+          backgroundImageSrc: firstB, // Ensure correct URL format
         }}
       ></Slide>
 
 
 <Slide
         background={{
-          backgroundImageSrc: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e', // Ensure correct URL format
+          backgroundImageSrc: secondB, // Ensure correct URL format
           
        
         }}
@@ -49,12 +54,14 @@ const Slider = () => {
 
 <Slide
         background={{
-          backgroundImageSrc: 'https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?q=80&w=1420&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Ensure correct URL format
+          backgroundImageSrc:thirdB, // Ensure correct URL format
           
        
         }}
       ></Slide>
+      <MenuNav/>
     </HeroSlider>
+    
   );
 };
 
